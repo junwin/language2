e2e01042256d09e470d454a377cd6fe8bdcd88b0
junwin <jdunwin@gmail.com>
2023-05-09 21:36:22 -0500

    fixes and regen requirements.txt

--- api_helpers.py
+++ api_helpers.py
@@ -42,7 +42,7 @@ def get_completion(prompt, temperature=0, model="gpt-3.5-turbo", max_retries=3, 
                 messages=messages,
                 temperature=temperature,  # this is the degree of randomness of the model's output
             )
-            logging.info(f'get_completionend: {model}')
+            logging.info(f'get_completion end: {model}')
             return response.choices[0].message["content"]
             
         except RateLimitError as e:
